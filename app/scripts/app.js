59d46c2b6317fa13c488cdc8c890b338afe33e5d
Jany <jany@betacode.io>
2016-01-23 01:05:13 +0700

    Generate dashboard page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,12 +9,18 @@ angular.module('foosballApp', [
 ])
   .config(function ($stateProvider, $urlRouterProvider, $compileProvider) {
     //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
     $urlRouterProvider.otherwise('/');
     $stateProvider
-      .state('index', {
-        url: '/',
+      .state('main', {
+        url: '/main',
         templateUrl: 'views/main.html',
         controller:'MainCtrl'
+      })
+      .state('dashboard', {
+        url: '/',
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
       })
 
     // make href accept javascript:void(0) as a url
