dc54c02d75a4019936c630ad5e9f3d942e40bc5f
Jany <jany@betacode.io>
2016-01-23 01:17:31 +0700

    build header layout

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('foosballApp')
+  .controller('HeaderCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
