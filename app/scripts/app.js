9a46f6ae886da6e70a55b14befbc2ba7671eee8c
Jany <jany@betacode.io>
2016-01-23 23:21:53 +0700

    Config rest angular js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,15 +5,16 @@ angular.module('foosballApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'restangular'
 ])
   .config(function (
     $stateProvider,
     $urlRouterProvider,
+    RestangularProvider,
     $compileProvider
   ) {
     //delete $httpProvider.defaults.headers.common['X-Requested-With'];
-
 
     $stateProvider
       .state('main', {
@@ -30,6 +31,10 @@ angular.module('foosballApp', [
     // For any unmatched url, redirect to /
     $urlRouterProvider.otherwise('/');
 
+    // Config Restangular
+    RestangularProvider.setBaseUrl('/api/');
+
     // make href accept javascript:void(0) as a url
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|javascript):/);
+
   });
