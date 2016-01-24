55fe73ac0885e9334f22e01d9db03d6f374e9586
Jany <jany@betacode.io>
2016-01-24 11:39:30 +0700

    Get mock rank data

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,17 +1,37 @@
 'use strict';
 
 angular.module('foosballApp')
-  .controller('DashboardCtrl', function ($scope, DashboardService) {
+  .controller('DashboardCtrl', function ($scope, GameService, RankService) {
 
     // Get Last Games
     $scope.getLastGames = function () {
-      DashboardService
+      GameService
         .getLastGames()
         .then(function (resp) {
-          console.log(resp.data);
-          $scope.matchesData = resp.data;
+          $scope.matchesData = resp.results;
+        }, function () {
+          $scope.errorMessage = 'Can not get data';
         });
     };
 
     $scope.getLastGames();
+
+    // Get players/teams ranking
+    $scope.getRank = function (param) {
+      RankService
+        .getRank(param)
+        .then(function (resp) {
+          $scope.rankData = resp.results;
+        }, function () {
+          $scope.errorMessage = 'Can not get data';
+        });
+    };
+
+    $scope.getRank('players');
+
+    // Switch tabs call loadRank again
+    $scope.loadRank = function(param) {
+      $scope.type = param;
+      $scope.getRank(param);
+    }
   });
