4059a77919cbbceb230fc90d4fbe7290c6df1da6
Jany <jany@betacode.io>
2016-01-24 23:06:08 +0700

    Add

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -2,9 +2,34 @@
 
 angular.module('foosballApp')
   .controller('HeaderCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    /**
+     * click on button new game
+     * show content newgame template
+    */
+    $scope.isShowNewGame = false;
+
+    // Change the name button creategame base on click Create Game
+    $scope.optionCreateBtn = function () {
+      $scope.strOptionCreate = $scope.isShowNewGame ? 'x Close' :'+ New Game';
+    };
+
+    $scope.optionCreateBtn();
+
+    /**
+     * Show template of create new game
+     * newgame template will close
+    */
+    $scope.showNewGame = function () {
+      $scope.isShowNewGame = !$scope.isShowNewGame;
+      $scope.optionCreateBtn();
+    };
+
+    /**
+     * when finished create a new game
+     * newgame template will close
+    */
+    $scope.createGame = function () {
+      $scope.isShowNewGame = false;
+      $scope.optionCreateBtn();
+    };
   });
