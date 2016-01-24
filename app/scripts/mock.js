dc13a30b6693d6ab6b2d53fefa88935cc108283a
Jany <jany@betacode.io>
2016-01-24 07:39:26 +0700

    Render mock data

--- app/scripts/mock.js
+++ app/scripts/mock.js
@@ -5,8 +5,52 @@ var nobackend = document.URL.match(/nobackend/);
 /** Endpoint */
 var startMock = function ($httpBackend) {
   $httpBackend.whenGET('/api/games').respond(function () {
-    var data = [];
-    return [200, {data: data}, {}];
+    var limitMatches = 5,
+        limitDays = 3,
+        matches = [],
+        days = [],
+        dataByMonth;
+
+      /**
+      * Render matches data
+      *
+      * times render: limitMatches
+      * @return {matches}
+      */
+    _.each(_.range(limitMatches), function (index) {
+      matches.push({
+        id: index + 1,
+        "team_1": {
+          "point": Faker.random.number(10),
+          "members": {"user_1": Faker.Name.firstName(), "user_2": Faker.Name.firstName()}
+        },
+        "team_2": {
+          "point": Faker.random.number(10),
+          "members": {"user_1": Faker.Name.firstName(), "user_2": Faker.Name.firstName()}
+        }
+      })
+    });
+
+    /**
+    * Render matches by days data
+    *
+    * times render: limitDays
+    * @return {matches by days}
+    */
+    _.each(_.range(limitDays), function (index) {
+      days.push({
+        "created_at": Faker.Date.recent(index),
+        "matches": matches
+      })
+    });
+
+    // The results send to dislay
+    dataByMonth = {
+        "month": Faker.Date.recent(limitDays),
+        "days": days
+      };
+
+    return [200, {data: dataByMonth}, {}];
   });
 
   // If GET it is not api it will this passThrough
