315ec39fcc7fea184a8df1b4483a7c9343793adc
Jany <jany@betacode.io>
2016-01-27 01:17:07 +0700

    Add behavior for create a user and signin

--- app/scripts/controllers/access.js
+++ app/scripts/controllers/access.js
@@ -2,9 +2,43 @@
 
 angular.module('foosballApp')
   .controller('AccessCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.retrySubmit = false;
+    $scope.retrySignIn = false;
+    /**
+     * Create user
+     * member contain:
+     * = firstName (required min-length: 6)
+     * = lastName (required min-length: 6)
+     * = email address (required need to have @roomorama.com)
+     * = password (required min-length: 6)
+    */
+    $scope.createUser = function (formNewMember, member) {
+
+      $scope.retrySubmit = true;
+      $scope.retrySignIn = false;
+
+      if (formNewMember.$valid) {
+        console.log('valid');
+      }
+      // that ra thi nen goi api
+      console.log(formNewMember, member);
+    };
+
+    /**
+     * Sign in
+     * user contain:
+     * = email address (required need to have @roomorama.com)
+     * = password (required min-length: 6)
+    */
+    $scope.signin = function (formSignin, user) {
+
+      $scope.retrySubmit = false;
+      $scope.retrySignIn = true;
+
+      if (formSignin.$valid) {
+        console.log('valid');
+      }
+      // that ra thi nen goi api
+      console.log(formSignin, user);
+    };
   });
